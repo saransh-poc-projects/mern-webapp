cdb1ef177e8d8ea3838e210e1b0bf8bc1e8b148f
Saransh Bhatt <saransh.qui@gmail.com>
2021-01-23 23:37:12 +0530

    Add files via upload

---
+++ frontend/src/data.js
@@ -0,0 +1,94 @@
+const data ={
+    products:[
+        {
+            _id : '1',
+            category: 'Service Subscription',
+            name:'Maid Subscription',
+            image:'/images/p1.jpg',
+            price: 5,
+            brand: 'ChinmayiiBaai',
+            Rating: '4',
+            numReviews:10,
+            description: 'High quality service'
+        },
+        {
+            _id : '2',
+            category: 'Service Subscription',
+            name:'Maid Subscription',
+            image:'/images/p2.jpg',
+            price: 10,
+            brand: 'SamarthBaai',
+            Rating: '2',
+            numReviews:10,
+            description: 'High quality service'
+        },
+        {
+            _id : '3',
+            category: 'Service Subscription',
+            name:'Maid Subscription',
+            image:'/images/p3.jpg',
+            price: 350,
+            brand: 'BaaiBaai',
+            Rating: '4',
+            numReviews:10,
+            description: 'High quality service'
+        },
+        {
+            _id : '4',
+            category: 'Service Subscription',
+            name:'Maid Subscription',
+            image:'/images/p4.jpg',
+            price: 200,
+            brand: 'AuntyBaai',
+            Rating: '4.5',
+            numReviews:10,
+            description: 'High quality service'
+        },
+        {
+            _id : '5',
+            category: 'Service Subscription',
+            name:'Maid Subscription',
+            image:'/images/p5.jpg',
+            price: 3000,
+            brand: 'SmartBaai',
+            Rating: '5',
+            numReviews:10,
+            description: 'High quality service'
+        },
+        {
+            _id : '6',
+            category: 'Service Subscription',
+            name:'Maid Subscription',
+            image:'/images/p6.jpg',
+            price: 150,
+            brand: 'SmartBaai',
+            Rating: '3',
+            numReviews:10,
+            description: 'High quality service'
+        },
+        {
+            _id : '7',
+            category: 'Service Subscription',
+            name:'Maid Subscription',
+            image:'/images/p1.jpg',
+            price: 5,
+            brand: 'ChinmayiiBaai',
+            Rating: '4',
+            numReviews:10,
+            description: 'High quality service'
+        },
+        {
+            _id : '8',
+            category: 'Service Subscription',
+            name:'Maid Subscription',
+            image:'/images/p2.jpg',
+            price: 10,
+            brand: 'SamarthBaai',
+            Rating: '2',
+            numReviews:10,
+            description: 'High quality service'
+        }
+    ]       
+    
+}
+export default data;
