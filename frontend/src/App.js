78f34b659fefd097dfe2060f1a6b0e14ae3e4fe6
saransh93 <saransh.qui@gmail.com>
2021-01-24 15:49:40 +0530

    product rating commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import data from './data';
+import data from './data'; 
+import Product from './components/Product'
 
 function App() {
   return (
@@ -17,26 +18,7 @@ function App() {
         <div class="row center">
           {
             data.products.map(product => (
-              <div key={product._id} class="card">
-              <a href="product.html">
-                  <img class="medium" src={product.image} alt={product.name}/>
-              </a>
-              <div class="card-body">
-                  <a href="product.html">
-                      <h2>{product.brand}</h2>
-                  </a>    
-                  <div class="rating">
-                      <span><i class="fa fa-star"></i></span>
-                      <span><i class="fa fa-star"></i></span>
-                      <span><i class="fa fa-star"></i></span>
-                      <span><i class="fa fa-star"></i></span>
-                      <span><i class="fa fa-star"></i></span>
-                  </div>
-                  <div class="price">
-                      Rs. {product.price}/ month        
-                  </div>
-              </div>                        
-          </div> 
+              <Product key={product._id} product={product}></Product>
             ))
           }
             
@@ -48,4 +30,3 @@ function App() {
 }
 
 export default App;
-
