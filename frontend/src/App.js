b43d2e69fd7b8482918382ad40f035566b7ac6ea
saransh93 <saransh.qui@gmail.com>
2021-02-07 13:35:45 +0530

    updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import data from './data'; 
-import Product from './components/Product'
+import { BrowserRouter,Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 
 function App() {
   return (
+    <BrowserRouter>
     <div class="grid-container">
     <header class="row">
         <div>
@@ -15,17 +18,12 @@ function App() {
         </div>
     </header>
     <main>
-        <div class="row center">
-          {
-            data.products.map(product => (
-              <Product key={product._id} product={product}></Product>
-            ))
-          }
-            
-        </div>
+      <Route path="/product/:id" component={ProductScreen}></Route>
+      <Route path="/" component={HomeScreen} exact></Route>
     </main>
     <footer class="row center">All Rights Reserved</footer>
 </div>
+</BrowserRouter>
   );
 }
 
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -5,7 +5,7 @@ export default function Product(props) {
     const { product } = props;
     return(
         <div key={product._id} class="card">
-              <a href="product.html">
+              <a href={product/product._id}>
                   <img class="medium" src={product.image} alt={product.name}/>
               </a>
               <div class="card-body">
